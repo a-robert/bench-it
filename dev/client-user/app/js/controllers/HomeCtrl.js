9c2f58c12f922c581a1a9d8986dcd5780f6a1dee
Robert Asaturyan <asaturyan.robert@gmail.com>
2014-10-08 15:25:17 +0400

    Initial commit for 4uckbench-it

---
+++ dev/client-user/app/js/controllers/HomeCtrl.js
@@ -0,0 +1,7 @@
+define(['controllers'], function(controllers) {
+    controllers.controller('HomeCtrl', ['$scope',
+        function ($scope) {
+            $scope.wellcome = '4uck_all';
+        }
+    ]);
+});
---
+++ dev/client-user/app/js/routes.js
@@ -0,0 +1,16 @@
+'use strict';
+
+define([
+    'angular',
+    'app',
+    'controllers/HomeCtrl'
+], function(angular, app) {
+
+    return app.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/home', {
+            templateUrl: 'app/partials/home.html',
+            controller: 'HomeCtrl'
+        });
+        $routeProvider.otherwise({redirectTo: '/home'});
+    }]);
+});
